42e43e984418b9e98f6fe701ee496dd6e76e8c60
Frida <55127994+johanssoncfrida@users.noreply.github.com>
2020-12-10 12:59:31 +0100

    fixed button

--- src/views/quizView.js
+++ src/views/quizView.js
@@ -15,12 +15,12 @@ const QuizView = ({ question, questionNr, handleClick }) => {
       {question &&
         question.answerOptions.map((op) => (
           <div className="row center" key={op.id}>
-            <a
+            <button
               className="btn btn-large no-uppercase"
               onClick={() => handleClick(op.isCorrect)}
             >
               {op.answerText}
-            </a>
+            </button>
           </div>
         ))}
     </div>
